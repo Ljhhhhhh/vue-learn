ddc571e8868a845c22737fe3f76fa1d284a54b09
ljhhhhhh <13216698987@163.com>
2020-06-08 16:12:06 +0800

    整理

--- src/nlrx-wjc/Observer.js
+++ src/nlrx-wjc/Observer.js
@@ -1,5 +1,76 @@
 // 源码位置：src/core/observer/index.js
 import Dep from './Dep'
+
+const arrayProto = Array.prototype
+// 创建一个对象作为拦截器
+export const arrayMethods = Object.create(arrayProto)
+
+// 改变数组自身内容的7个方法
+const methodsToPatch = [
+  'push',
+  'pop',
+  'shift',
+  'unshift',
+  'splice',
+  'sort',
+  'reverse',
+]
+
+/**
+ * Intercept mutating methods and emit events
+ */
+methodsToPatch.forEach(function (method) {
+  const original = arrayProto[method] // 缓存原生方法
+  Object.defineProperty(arrayMethods, method, {
+    enumerable: false,
+    configurable: true,
+    writable: true,
+    value: function mutator(...args) {
+      const result = original.apply(this, args)
+      const ob = this.__ob__
+      let inserted
+      switch (method) {
+        case 'push':
+        case 'unshift':
+          inserted = args // 如果是push或unshift方法，那么传入参数就是新增的元素
+          break
+        case 'splice':
+          inserted = args.slice(2) // 如果是splice方法，那么传入参数列表中下标为2的就是新增的元素
+          break
+      }
+      if (inserted) ob.observeArray(inserted) // 调用observe函数将新增的元素转化成响应式
+      // notify change
+      ob.dep.notify()
+      return result
+    },
+  })
+})
+
+// 能力检测：判断__proto__是否可用，因为有的浏览器不支持该属性
+export const hasProto = '__proto__' in {}
+
+const arrayKeys = Object.getOwnPropertyNames(arrayMethods)
+
+/**
+ * Augment an target Object or Array by intercepting
+ * the prototype chain using __proto__
+ */
+function protoAugment(target, src, keys) {
+  target.__proto__ = src
+}
+
+/**
+ * Augment an target Object or Array by defining
+ * hidden properties.
+ */
+/* istanbul ignore next */
+function copyAugment(target, src, keys) {
+  for (let i = 0, l = keys.length; i < l; i++) {
+    const key = keys[i]
+    // def(target, key, src[key])
+  }
+}
+
 /**
  * Observer类会通过递归的方式把一个对象的所有属性都转化成可观测对象
  */
@@ -10,8 +81,9 @@ export class Observer {
     // 相当于为value打上标记，表示它已经被转化成响应式了，避免重复操作
     // def(value, '__ob__', this)
     if (Array.isArray(value)) {
-      // 当value为数组时的逻辑
-      // ...
+      const augment = hasProto ? protoAugment : copyAugment
+      augment(value, arrayMethods, arrayKeys)
+      this.observeArray(value)
     } else {
       this.walk(value)
     }
@@ -24,6 +96,15 @@ export class Observer {
       defineReactive(obj, keys[i])
     }
   }
+
+  /**
+   * Observe a list of Array items.
+   */
+  observeArray(items) {
+    for (let i = 0, l = items.length; i < l; i++) {
+      observe(items[i])
+    }
+  }
 }
 /**
  * 使一个对象转化成可观测对象
@@ -32,6 +113,7 @@ export class Observer {
  * @param { Any } val 对象的某个key的值
  */
 function defineReactive(obj, key, val) {
+  let childOb = observe(val)
   // 如果只传了obj和key，那么val = obj[key]
   if (arguments.length === 2) {
     val = obj[key]
@@ -48,6 +130,9 @@ function defineReactive(obj, key, val) {
       window.target = this
       console.log('获取数据')
       dep.depend() // 收集依赖
+      if (childOb) {
+        childOb.dep.depend()
+      }
       return val
     },
     set(newVal) {
@@ -59,4 +144,24 @@ function defineReactive(obj, key, val) {
       dep.notify() // 在setter中通知依赖更新
     },
   })
+}
+
+/**
+ * Attempt to create an observer instance for a value,
+ * returns the new observer if successfully observed,
+ * or the existing observer if the value already has one.
+ * 尝试为value创建一个0bserver实例，如果创建成功，直接返回新创建的Observer实例。
+ * 如果 Value 已经存在一个Observer实例，则直接返回它
+ */
+export function observe(value, asRootData) {
+  if (typeof value != 'object' || value instanceof VNode) {
+    return
+  }
+  let ob
+  if (hasOwn(value, '__ob__') && value.__ob__ instanceof Observer) {
+    ob = value.__ob__
+  } else {
+    ob = new Observer(value)
+  }
+  return ob
 }
